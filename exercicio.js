f37b1f763fc81e448d94b2aa54eb91c243f68727
MARCELINO RODRIGUES JUNIOR <marcelino_rodrigu@estudante.sesisenai.org.br>
2025-04-17 21:07:11 -0300

    add exercicio

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+function somar(){
+    let n1, n2, soma
+
+    n1 = Number ( prompt ("Primeiro numero: "))
+    n2 = Number (prompt ("Segundo numero: "))
+
+    soma = n1 + n2 
+
+    alert("Soma: " + soma)
+}
+
+function diminuir(){
+    let n1, n2 , diferenca
+    n1 = Number (prompt("Primeiro numero: "))
+    n2 = Number (prompt("Segundo numero: "))
+    diferenca = n1 - n2
+    alert("Diferenca: " + diferenca)
+}
