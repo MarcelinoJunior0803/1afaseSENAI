a1f4dfae72df379b763e9d7c051d72828bbf9e1c
MARCELINO RODRIGUES JUNIOR <marcelino_rodrigu@estudante.sesisenai.org.br>
2025-04-22 21:56:27 -0300

    Atividades nova alex

---
+++ atividade alex/atividade_alex.js
@@ -0,0 +1,14 @@
+function exercicio1(){
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite outro número :"))
+    let soma = num1 + num2
+    alert (`A soma dos dois números é: ${soma}`)
+    alert("A soma do dois números é: " + soma)
+}
+function exercicio2(){
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite outro número :"))
+    let media = (num1 + num2) / 2
+    alert("A média dos dois número é: " + media)
+}
+
---
+++ atividades para entragr alex/ativ1alex.js
@@ -0,0 +1,6 @@
+// 1. Exercício 1 - Saudação Personalizada
+function Saudaçao(){
+    let name = prompt("Digite seu nome")
+    
+    alert("Óla, "+ name+"!"+"Seja Bem Vindo")
+}
