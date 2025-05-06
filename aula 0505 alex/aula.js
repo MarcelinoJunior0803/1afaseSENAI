7f2919fceb947074309767c019f377ea387ab779
MARCELINO RODRIGUES JUNIOR <marcelino_rodrigu@estudante.sesisenai.org.br>
2025-05-05 22:02:04 -0300

    Aula Alex 0505

---
+++ aula 0505 alex/aula.js
@@ -0,0 +1,26 @@
+//1. Mostre apenas os números pares de 1 a 20.
+function Par(){
+let numero = 1
+ while(numero <= 20){
+     let resto = numero % 2
+     if(resto === 0) alert("Número: " + numero + " é Par")
+     numero++
+ }
+}
+//2. Mostre apenas os números ímpares de 1 a 20.
+function Ímpar(){
+let numero = 1
+while(numero <= 20){
+    let resto = numero % 2
+    if(resto != 0) alert("Número: " + numero + " é Ímpar")
+    numero++
+ }
+} 
+//3. Imprima “Olá, mundo!” 5 vezes.
+function ólamundo(){
+    let frase = 1
+    while(frase <= 5) {
+        alert("Óla Mundo!")
+    frase++
+    }
+}
